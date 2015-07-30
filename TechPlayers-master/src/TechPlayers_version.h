a471565f2643af8d500ea27b781067be04fec683
TechPlayers <kiran.barnana@verizon.com>
2015-07-30 01:41:26 -0700

    chnaged
    chnaged

---
+++ TechPlayers-master/src/TechPlayers_version.h
@@ -0,0 +1,14 @@
+/**
+ * @file TechPlayers_version.h
+ * Simply defines TechPlayers_VERSION.
+ * I don't particularly like how autoconf and friends handle the version...
+ *
+ * @author  Ben Gardner
+ * @license GPL v2+
+ */
+#ifndef TechPlayers_VERSION_H_INCLUDED
+#define TechPlayers_VERSION_H_INCLUDED
+
+#define TechPlayers_VERSION    "@PACKAGE_VERSION@"
+
+#endif /* TechPlayers_VERSION_H_INCLUDED */
